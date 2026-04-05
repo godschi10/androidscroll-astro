5af9b2e166e967347c3d03905b9617b612af09c7
godschi10 <godschi10@gmail.com>
2026-04-05 21:30:50 +0100

    fix sitemap site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://androidscroll.com',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [mdx(), sitemap()]
 });
